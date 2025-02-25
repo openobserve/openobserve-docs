b9899be8711963f1d17a8a43c48be79ea050bd49
Prabhat Sharma <hi.prabhat@gmail.com>
2025-02-25 12:12:03 -0800

    changed to new bucket

--- deploy2.sh
+++ deploy2.sh
@@ -7,7 +7,7 @@ rm -rf site
 mkdocs build
 
 # Move the files to S3 bucket for hosting
-aws s3 sync ./site s3://zincsearch-website/docs  --exclude=".git/*" --profile=o2-prod
+aws s3 sync ./site s3://openobserve-prod-website/docs  --exclude=".git/*" --profile=o2-prod
 
 # invalidate cloudfront cache so that latest files can be served
 aws cloudfront create-invalidation --distribution-id E3KB2JN7JA8U38 --paths="/docs/*" --profile=o2-prod
